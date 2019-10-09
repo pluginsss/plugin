5a1c7bed7975955977118039031d83d8da2811a5
plugsss <1540623640@qq.com>
2019-10-09 22:44:26 +0800

    复数的运算

--- xiaolusss.java
+++ xiaolusss.java
@@ -1,25 +1,109 @@
-public class xiaolusss{
-	static class Julong{//巨龙攻击力
-		float a = 1;
+package xiaolu;
+import java.util.Scanner;
+ 
+class Complex { // 复数类
+	double real;  // 实部
+	double image; // 虚部
+	
+	Complex(){  // 不带参数的构造方法
+		Scanner input = new Scanner(System.in);
+		double real = input.nextDouble();
+		double image = input.nextDouble();
+		Complex(real,image);
 	}
-	static class Shenxianshui{//血瓶回复指数
-		float ip = 2;
+ 
+	private void Complex(double real, double image) { // 供不带参数的构造方法调用
+		// TODO Auto-generated method stub
+		this.real = real;
+		this.image = image;
 	}
-	public static void main (String[] args) {
-		float x;//女警血量
-		float i, j, z;
-		Julong jianxue = new Julong();
-		Shenxianshui jiaxue = new Shenxianshui();
-		for(i = 0; i < 5; i += jianxue.a) {//巨龙总伤害
-			;
+ 
+	Complex(double real,double image){ // 带参数的构造方法
+		this.real = real;
+		this.image = image;
+	}
+ 
+	public double getReal() {
+		return real;
+	}
+ 
+	public void setReal(double real) {
+		this.real = real;
+	}
+ 
+	public double getImage() {
+		return image;
+	}
+ 
+	public void setImage(double image) {
+		this.image = image;
+	}
+	
+	Complex add(Complex a){ // 复数相加
+		double real2 = a.getReal();
+		double image2 = a.getImage();
+		double newReal = real + real2;
+		double newImage = image + image2;
+		Complex result = new Complex(newReal,newImage);
+		return result;
+	}
+	
+	Complex sub(Complex a){ // 复数相减
+		double real2 = a.getReal();
+		double image2 = a.getImage();
+		double newReal = real - real2;
+		double newImage = image - image2;
+		Complex result = new Complex(newReal,newImage);
+		return result;
+	}
+	
+	Complex mul(Complex a){ // 复数相乘
+		double real2 = a.getReal();
+		double image2 = a.getImage();
+		double newReal = real*real2 - image*image2;
+		double newImage = image*real2 + real*image2;
+		Complex result = new Complex(newReal,newImage);
+		return result;
+	}
+	
+	Complex div(Complex a){ // 复数相除
+		double real2 = a.getReal();
+		double image2 = a.getImage();
+		double newReal = (real*real2 + image*image2)/(real2*real2 + image2*image2);
+		double newImage = (image*real2 - real*image2)/(real2*real2 + image2*image2);
+		Complex result = new Complex(newReal,newImage);
+		return result;
+	}
+	
+	public void print(){ // 输出
+		if(image > 0){
+			System.out.println(real + " + " + image + "i");
+		}else if(image < 0){
+			System.out.println(real + "" + image + "i");
+		}else{
+			System.out.println(real);
 		}
-		for(j = 1; j < 2*2*2*2*2; j *= jiaxue.ip) {//血瓶总治疗倍数
-			;
-		}
-		x = i / j;
-		z = i - x;
-		System.out.println("巨龙总伤害为： " + i);
-		System.out.println("血瓶总治疗量为：" + z);
-		System.out.println("女警血量为：" + x);
+	}
+}
+
+public class Xiaolusss { // 用于测试复数类
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("请用户输入第一个复数的实部和虚部:");
+        Complex data1 = new Complex();
+        System.out.println("请用户输入第二个复数的实部和虚部:");
+        Complex data2 = new Complex();
+       
+        // 以下分别为加减乘除
+        Complex result_add = data1.add(data2);
+        Complex result_sub = data1.sub(data2);
+        Complex result_mul = data1.mul(data2);
+        Complex result_div = data1.div(data2);
+        
+        result_add.print();
+        result_sub.print();
+        result_mul.print();
+        result_div.print();
 	}
 }
